432c690defabd62ebfad68745891a9f9336baf18
D1R1R <104569323+D1R1R@users.noreply.github.com>
2023-04-25 08:14:00 +0300

    end

--- analyzer.py
+++ analyzer.py
@@ -7,6 +7,25 @@ syntax_parser = natasha.NewsSyntaxParser(emb)
 ner_tagger = natasha.NewsNERTagger(emb)
 morph_vocab = natasha.MorphVocab()
 
+antonims = {
+    "отец": "сын:дочь",
+    "папа": "сын:дочь",
+    "мать": "сын:дочь",
+    "мама": "сын:дочь",
+    "дети": "сын:дочь",
+    "родители": "отец:мать",
+    "дочь": "отец:мать",
+    "сын": "отец:мать",
+    "дочка": "отец:мать",
+    "сын": "отец:мать",
+    "жена": "муж",
+    "муж": "жена",
+    "дедушка": "бабушка",
+    "бабушка": "дедушка",
+    "брат": "сестра",
+    "сестра": "брат",
+}
+
 def choose_type(text: str):
     request_type = None
 
@@ -22,6 +41,7 @@ def choose_type(text: str):
 def find_names(text: str):
     doc = get_start(text)
     markup = syntax_parser(text.split())
+    # dictionary of dependent words
     markup_dict = {_.id: _.text for _ in markup.tokens}
     for span in doc.spans:
         for token in markup.tokens:
@@ -29,9 +49,20 @@ def find_names(text: str):
                 if els in token.text:
                     span.normalize(morph_vocab)
                     if token.head_id in markup_dict.keys():
-                        return markup_dict[token.head_id].lower(), span.normal
+                        code_name = markup_dict[token.head_id].lower()
+                        # Case checking only for persons
+                        if span.type == 'PER':
+                            for sent in doc.sents:
+                                for tokens in sent.morph.tokens:
+                                    # Case checking
+                                    if tokens.pos == 'PROPN' and tokens.feats['Case'] == 'Gen':
+                                        try:
+                                            code_name = antonims[code_name]
+                                            break
+                                        except:
+                                            pass
+                        return code_name, span.normal
                     return span.normal
-                    break
 
 def get_start(text: str):
     doc = natasha.Doc(text)
